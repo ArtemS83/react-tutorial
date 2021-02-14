2556194a53857c950af2e58277a1d9d86f1f6d5c
Atrem Sergieiev <a_serg83@ukr.net>
2021-02-14 18:51:43 +0100

    3commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from './Title';
+// import Title from './Title';
 import Text from './Text';
 const App = () => (
   <div>
--- src/Text.jsx
+++ src/Text.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Text = ({ text }) => (
     <div>
         <Number num={1000}/>
-        <p>Some { text1}</p>
+        <p>Some { text}</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo quibusdam quos corrupti mollitia commodi voluptatibus beatae at placeat quam atque nam delectus dignissimos, excepturi odio, necessitatibus, et debitis consequuntur.
         </p>
     </div>
