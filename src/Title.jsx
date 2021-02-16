3e853b69b88373571125ebfcbf4382ee168493a5
Atrem Sergieiev <a_serg83@ukr.net>
2021-02-16 14:08:52 +0100

    add

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Title.module.css'
+// import './Title.css'
+console.log(s);
 const Title = (props) => (
-    <h1>Hello {props.title}!</h1>
+    <>
+     <h1 className={s.Title + ' ' + s.ColorRed}  >Hello {props.title}!</h1> 
+    <h2 className={`${s.Title} ${s.ColorRed}`}  >Bay {props.title}!</h2> 
+     {/* <h2 className={`Title Color-Red`}  >DDDD {props.title}!</h2> */}
+    </>
 )
+
 Title.defaultProps = {
     title: "REACT",
 }
+
 Title.propTypes = {
     title: PropTypes.string,
 };
